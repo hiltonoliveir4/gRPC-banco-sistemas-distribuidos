44614be4df0bf8bfeccd78354fd33dde861944c9
Rafael Fernandes Lopes <rafael.fernandes@gmail.com>
2021-07-16 21:47:33 -0300

    Implementados o cliente e o servidor com as operações Extrato, Saque e Deposito

---
+++ client.js
@@ -0,0 +1,84 @@
+const PROTO_PATH = './banco.proto';
+
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
+// Suggested options for similarity to existing grpc.load behavior
+const packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition).banco;
+
+const client = new protoDescriptor.ServicoBancario("127.0.0.1:50051", grpc.credentials.createInsecure());
+
+// client.Saque({
+//     valor: 100.0,
+//     nomeCaixa: "CAIXA SD",
+// }, (err, response) => {
+//     const transacao = response;
+
+//     if (transacao.codigo == -1) {
+//         console.log("Operação de saque falhou!");
+//     } else {
+//         console.log("Operação de saque realizada com sucesso!");
+//     }
+
+//     console.log(transacao.codigo);
+//     console.log(transacao.valor);
+//     console.log(transacao.descricao);
+//     console.log(transacao.data);
+
+//     client.Saque({
+//         valor: 50.0,
+//         nomeCaixa: "CAIXA SD",
+//     }, (err, response) => {
+//         const transacao = response;
+    
+//         if (transacao.codigo == -1) {
+//             console.log("Operação de saque falhou!");
+//         } else {
+//             console.log("Operação de saque realizada com sucesso!");
+//         }
+    
+//         console.log(transacao.codigo);
+//         console.log(transacao.valor);
+//         console.log(transacao.descricao);
+//         console.log(transacao.data);
+    
+//     });
+
+// });
+
+// client.Deposito({
+//     valor: 70.0,
+//     nomeCaixa: "CAIXA SD",
+// }, (err, response) => {
+//     const transacao = response;
+
+//     console.log("Operação de depósito realizada com sucesso!");
+
+//     console.log(transacao.codigo);
+//     console.log(transacao.valor);
+//     console.log(transacao.descricao);
+//     console.log(transacao.data);
+// });
+
+client.Extrato({}, (err, response) => {
+    const listaDeTransacoes = response.transacoes;
+
+    console.log("EXTRATO DO BANCO SD S.A.\n");
+
+    for (i = 0; i < listaDeTransacoes.length; i++) {
+        const transacao = listaDeTransacoes[i];
+
+        console.log(`${transacao.codigo}   ${transacao.descricao}\t${transacao.data}  R\$ ${transacao.valor}`);
+    }
+
+    console.log("\n------ FIM DO EXTRATO");
+})
---
+++ server.js
@@ -0,0 +1,109 @@
+const PROTO_PATH = './banco.proto';
+
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
+// Suggested options for similarity to existing grpc.load behavior
+const packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition).banco;
+
+// The protoDescriptor object has the full package hierarchy
+const servicoBancario = protoDescriptor.ServicoBancario;
+
+const bd = {
+    saldo: 1000.0,
+    idTransacao: 1,
+    trasacoes: [],
+};
+
+const server = new grpc.Server();
+
+// implementar o serviço
+server.addService(servicoBancario.service, {
+    // rpc Extrato (Vazio) returns (ListaDeTransacoes);
+    // rpc Saque (DadosOperacao) returns (Transacao);
+    // rpc Deposito (DadosOperacao) returns (Transacao);
+    // rpc Transferencia (DadosOperacao) returns (Transacao);
+    // rpc Compra (DadosOperacao) returns (Transacao);
+
+    Extrato : function(call, callback) {
+        callback(null, { transacoes: bd.trasacoes } );
+    },
+
+
+    Saque : function(call, callback) {
+        const dadosOperacao = call.request;
+
+        const valor = dadosOperacao.valor;
+        const nomeCaixa = dadosOperacao.nomeCaixa;
+
+        if (bd.saldo >= valor) {
+            bd.saldo -= valor;
+            bd.idTransacao++;
+
+            const transacao = {
+                codigo: bd.idTransacao,
+                valor: valor,
+                descricao: `SAQUE REALIZADO NO TERMINAL ${nomeCaixa}`,
+                data: Date.now().toString(),
+            };
+
+            bd.trasacoes.push(transacao);
+
+            console.log("Saldo atual: " + bd.saldo);
+
+            callback(null, transacao);
+        } else {
+            const transacao = {
+                codigo: -1,
+                valor: 0.0,
+                descricao: `TENTATIVA DE SAQUE NO TERMINAL ${nomeCaixa} FRUSTRADA FUÊ!`,
+                data: Date.now().toString(),
+            };
+
+            console.log("Saldo atual: " + bd.saldo);
+
+            callback(null, transacao);
+        }
+    },
+    Deposito : function(call, callback) {
+        const dadosOperacao = call.request;
+
+        const valor = dadosOperacao.valor;
+        const nomeCaixa = dadosOperacao.nomeCaixa;
+
+        bd.saldo += valor;
+        bd.idTransacao++;
+
+        const transacao = {
+            codigo: bd.idTransacao,
+            valor: valor,
+            descricao: `DEPOSITO REALIZADO NO TERMINAL ${nomeCaixa}`,
+            data: Date.now().toString(),
+        };
+
+        bd.trasacoes.push(transacao);
+
+        console.log("Saldo atual: " + bd.saldo);
+
+        callback(null, transacao);
+    },
+
+
+    Transferencia : function(call, callback) {},
+
+    Compra : function(call, callback) {},
+});
+
+server.bindAsync("0.0.0.0:50051", grpc.ServerCredentials.createInsecure(), (error, port) => {
+    console.log("Servidor gRPC rodando!");
+    server.start();
+});
